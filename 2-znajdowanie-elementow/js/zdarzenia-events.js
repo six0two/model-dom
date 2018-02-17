067f416c5f616d96860d9c3869cb66e406d96efd
six0two <eyescreem.022@gmail.com>
2018-02-17 15:59:31 +0100

    zdarzenia c.d.

--- 2-znajdowanie-elementow/js/zdarzenia-events.js
+++ 2-znajdowanie-elementow/js/zdarzenia-events.js
@@ -6,9 +6,37 @@ var secondLink = document.getElementsByTagName('a')[1];
 function alarm(e) {
     e.preventDefault(); //zapobiegamy domyślnej akcji
     console.log('kliknięto kolejny link');
-    
+    thirdLink.style.backgroundColor = "red";
     console.log(e.type);
 }
 
 secondLink.onclick = alarm;
 //wywołujemy funkcę alarm na drugim kliknięciu
+
+var thirdLink = document.getElementsByTagName('a')[2];
+//Tworzymy zmienną z trzecim linkiem
+
+thirdLink.addEventListener("click", alarm);
+
+//thirdLink.removeEventListener("click", alarm);
+
+
+
+// Efekt powiększenia napisu po najechaniu 
+var mainHeader= document.getElementById('main-header');
+// Przypisujemy nagłówek strony do zmiennej
+
+function resize(e) {
+    if(e.type == 'mouseover') {
+        mainHeader.style.fontSize = "70px";
+    }
+    else {
+        mainHeader.style.fontSize = "inherit"; //Przywróć domyślną wielkość po zjechaniu
+    }
+}
+
+mainHeader.onmouseover = resize;
+mainHeader.onmouseout = resize;
+
+
+
